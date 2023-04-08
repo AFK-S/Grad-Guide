ed03f58d3ba8dd36cb6aa0a76e6593c35bb5974f
Karan2310 <karandeep9619@gmail.com>
2023-04-08 21:08:28 +0530

    GIGs Updated

--- client/components/GIGS/AllGigs.js
+++ client/components/GIGS/AllGigs.js
@@ -1,8 +1,8 @@
-import { StyleSheet, Text, View,SafeAreaView,FlatList,TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View,SafeAreaView,FlatList,TouchableOpacity,Linking } from "react-native";
 import React from "react";
-import { CommonStyles } from "../CommonStyles";
+import { CommonStyles } from "../../CommonStyles";
 
-const Gigs = () => {
+const AllGigs = () => {
   // const [gigs, setGigs] = React.useState([]);
   
   const data = [
@@ -11,26 +11,28 @@ const Gigs = () => {
       "description": "Guys please make your repo names as djcsi_ Make it a private repo and add djcsi-codeshastra-9 as collaboratorsThis is a gig",
       "price": 100,
       "location": "Bangalore",
-      "user" : "userhere@gmail.com"
+      "user" : "userhere@gmail.com",
+      "contact_number": "1234567890"
     },
     {
       "title": "Gig 2",
       "description": "Guys please make your repo names as djcsi_ Make it a private repo and add djcsi-codeshastra-9 as collaboratorsThis is a gig",
       "price": 90,
       "location": "Bangalore",
-      "user" : "userhere@gmail.com"
+      "user" : "userhere@gmail.com",
+      "contact_number": "987654375"
     },
     {
       "title": "Gig 2",
       "description": "Guys please make your repo names as djcsi_ Make it a private repo and add djcsi-codeshastra-9 as collaboratorsThis is a gig",
       "price": 90,
       "location": "Bangalore",
-      "user" : "userhere@gmail.com"
+      "user" : "userhere@gmail.com",
+      "contact_number": "1234567890"
     },
   ]
 
   return (
-    <SafeAreaView style={CommonStyles.container}>
      <FlatList 
           keyExtractor={(item,index)=>index.toString()}
           showsVerticalScrollIndicator={false}
@@ -62,25 +64,22 @@ const Gigs = () => {
                 <Text style={{...CommonStyles.silentText,marginTop:20,fontWeight:"bold"}}>Posted By : {item.user}</Text>
                 <View>
 
-                <View style={{marginTop:15, display:"flex",flexDirection:"row",width:"100%",justifyContent:"space-between"}}>
-                <TouchableOpacity style={{...CommonStyles.outlineBlueBtn,width:"48%"}}>
-                  <Text style={{color:"#007bff",textAlign:"center",fontWeight:"bold"}}>Contact</Text>
+                <TouchableOpacity style={{...CommonStyles.outlineBlueBtn,width:"100%",marginTop:15}}
+                onPress={()=>{
+                  Linking.openURL(`tel:${item.contact_number}`);
+                }}
+                >
+                  <Text style={{...CommonStyles.blue,textAlign:"center",fontWeight:"bold"}}>Contact</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={{...CommonStyles.blueBtn,width:"48%"}}>
-                  <Text style={{color:"white",textAlign:"center",fontWeight:"bold"}}>Apply</Text>
-                </TouchableOpacity>
-                </View>
-
                 </View>
                </View>
               )
           }}
      />
-    </SafeAreaView>
   );
 };
 
-export default Gigs;
+export default AllGigs;
 
 const styles = StyleSheet.create({
   title:{
