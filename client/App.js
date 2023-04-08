c7bb8bccbae4e4a4eb80fe747ea273d21129f14e
SarthakDeshmukh13 <saksard@gmail.com>
2023-04-08 17:49:33 +0530

    Top Tab Navigation added

--- client/App.js
+++ client/App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Mainscreen from "./routes/Mainscreen";
+import News from "./components/News";
+import Social from "./components/Social";
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
--- client/components/Expenses.js
+++ client/components/Expenses.js
@@ -1,11 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 import React from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
+import Social from "../components/Social";
+import News from "../components/News";
+import Tracker from "./Tracker";
+import Statistics from "./Statistics";
+
+const Tab = createMaterialTopTabNavigator();
 const Expenses = () => {
   return (
-    <View>
-      <Text>Expenses</Text>
-    </View>
+    <Tab.Navigator style={{ paddingTop: StatusBar.currentHeight }}>
+      <Tab.Screen
+        options={{
+          title: ({ color, focused }) => (
+            <Ionicons
+              size={25}
+              name={focused ? "home" : "home-outline"}
+              color={focused ? "black" : "#272727"}
+            />
+          ),
+        }}
+        component={Tracker}
+        name="Tracker"
+      />
+      <Tab.Screen
+        options={{
+          title: ({ color, focused }) => (
+            <Ionicons
+              size={25}
+              name={focused ? "people-sharp" : "people-outline"}
+              color={focused ? "black" : "#272727"}
+            />
+          ),
+        }}
+        component={Statistics}
+        name="Statistics"
+      />
+    </Tab.Navigator>
   );
 };
 
