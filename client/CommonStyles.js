9fc8f487602922710a707b2f760478b9c78eb753
Karan2310 <karandeep9619@gmail.com>
2023-04-08 19:40:55 +0530

    GIGs Added

---
+++ client/CommonStyles.js
@@ -0,0 +1,46 @@
+export const CommonStyles = {
+    container: {
+        flex: 8 / 9,
+    },
+    card: {
+        marginBottom: 30,
+        backgroundColor: 'white',
+        padding: 25,
+        borderRadius: 15,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 6,
+        elevation: 5,
+    },
+    silentText:{
+        color:"#8e8e8e",
+        fontSize:13,
+    },
+    cardRow:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignItems:"center",
+        marginBottom:20,
+    },
+    divider:{
+        height:1,
+        backgroundColor:"#e0e0e0",
+        marginTop:20
+    },
+    blueBtn:{
+        backgroundColor:"#007bff",
+        padding:10,
+        borderRadius:10,
+    },
+    outlineBlueBtn:{
+        borderColor:"#007bff",
+        borderWidth:1,
+        padding:10,
+        borderRadius:10,
+    }
+};
+
--- client/components/Gigs.js
+++ client/components/Gigs.js
@@ -1,14 +1,96 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View,SafeAreaView,FlatList,TouchableOpacity } from "react-native";
 import React from "react";
+import { CommonStyles } from "../CommonStyles";
 
 const Gigs = () => {
+  // const [gigs, setGigs] = React.useState([]);
+  
+  const data = [
+    {
+      "title": "Gig 1",
+      "description": "Guys please make your repo names as djcsi_ Make it a private repo and add djcsi-codeshastra-9 as collaboratorsThis is a gig",
+      "price": 100,
+      "location": "Bangalore",
+      "user" : "userhere@gmail.com"
+    },
+    {
+      "title": "Gig 2",
+      "description": "Guys please make your repo names as djcsi_ Make it a private repo and add djcsi-codeshastra-9 as collaboratorsThis is a gig",
+      "price": 90,
+      "location": "Bangalore",
+      "user" : "userhere@gmail.com"
+    },
+    {
+      "title": "Gig 2",
+      "description": "Guys please make your repo names as djcsi_ Make it a private repo and add djcsi-codeshastra-9 as collaboratorsThis is a gig",
+      "price": 90,
+      "location": "Bangalore",
+      "user" : "userhere@gmail.com"
+    },
+  ]
+
   return (
-    <View>
-      <Text>Gigs</Text>
-    </View>
+    <SafeAreaView style={CommonStyles.container}>
+     <FlatList 
+          keyExtractor={(item,index)=>index.toString()}
+          showsVerticalScrollIndicator={false}
+     style={{padding:30}}
+          data={data}
+          renderItem={({item}) =>{
+              return(
+                <View style={CommonStyles.card}>
+                <View style={CommonStyles.cardRow}>
+                  <View>
+                  <Text style={styles.title}>
+                    {item.title}
+                  </Text>
+                  <Text style={CommonStyles.silentText}>
+                    {item.location}
+                  </Text>
+                  </View>
+                <View>
+                  <Text style={styles.price}>
+                  ₹ {item.price} /hr
+                  </Text>
+                </View>
+                </View>
+                <Text>
+                {item.description}
+                </Text>
+                <View style={CommonStyles.divider}></View>
+               
+                <Text style={{...CommonStyles.silentText,marginTop:20,fontWeight:"bold"}}>Posted By : {item.user}</Text>
+                <View>
+
+                <View style={{marginTop:15, display:"flex",flexDirection:"row",width:"100%",justifyContent:"space-between"}}>
+                <TouchableOpacity style={{...CommonStyles.outlineBlueBtn,width:"48%"}}>
+                  <Text style={{color:"#007bff",textAlign:"center",fontWeight:"bold"}}>Contact</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{...CommonStyles.blueBtn,width:"48%"}}>
+                  <Text style={{color:"white",textAlign:"center",fontWeight:"bold"}}>Apply</Text>
+                </TouchableOpacity>
+                </View>
+
+                </View>
+               </View>
+              )
+          }}
+     />
+    </SafeAreaView>
   );
 };
 
 export default Gigs;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title:{
+    fontSize:20,
+    fontWeight:"bold",
+    marginBottom:7,
+  },
+  price:{
+    fontSize:20,
+    fontWeight:"bold",
+    marginBottom:10,
+  }
+});
