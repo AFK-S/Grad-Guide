8788f4dd7e02b97806d9d0044bc74da1ee0cf9d0
adi50920 <adityarai50920@gmail.com>
2023-04-09 08:25:44 +0530

    Update Tracker

--- client/Screens/Tracker.js
+++ client/Screens/Tracker.js
@@ -9,132 +9,129 @@ import {
   ScrollView,
   Modal,
   TextInput,
-} from "react-native";
-import React, { useEffect, useContext, useState } from "react";
-import { CommonStyles } from "../CommonStyles";
-import axios from "axios";
-import { SERVER_URL } from "../config";
-import StateContext from "../context/StateContext";
-import * as DocumentPicker from "expo-document-picker";
-import RadioGroup from "react-native-radio-buttons-group";
-import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+} from 'react-native'
+import React, { useEffect, useContext, useState } from 'react'
+import { CommonStyles } from '../CommonStyles'
+import axios from 'axios'
+import { SERVER_URL } from '../config'
+import StateContext from '../context/StateContext'
+import * as DocumentPicker from 'expo-document-picker'
+import RadioGroup from 'react-native-radio-buttons-group'
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
 
 const Tracker = () => {
-  const { User, setLoading } = useContext(StateContext);
-  const [userData, setUserData] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false);
+  const { User, setLoading } = useContext(StateContext)
+  const [userData, setUserData] = useState([])
+  const [expenseData, setExpenseData] = useState([])
+  const [predictExpenseData, setPredictExpenseData] = useState([])
+  const [modalVisible, setModalVisible] = useState(false)
   const [expense, setExpense] = useState({
-    amount: "",
-    type_of_transaction: "",
+    amount: '',
+    type_of_transaction: '',
     user_id: User,
-  });
-  const transactions = [
-    {
-      _id: "1",
-      amount: 500,
-      category: "Food",
-      date: "12/12/2020",
-      description: "Bought food",
-    },
-    {
-      _id: "1",
-      amount: 500,
-      category: "Food",
-      date: "12/12/2020",
-      description: "Bought food",
-    },
-  ];
+  })
 
   useEffect(() => {
-    (async () => {
-      setLoading(true);
+    ;(async () => {
+      setLoading(true)
       try {
-        const { data } = await axios.get(`${SERVER_URL}/api/user/${User}`);
-        setUserData(data);
+        const { data } = await axios.get(`${SERVER_URL}/api/user/${User}`)
+        setUserData(data)
+        const { data: expenseData } = await axios.get(
+          `${SERVER_URL}/api/transactions/predict/${User}`,
+        )
+        setPredictExpenseData(expenseData)
       } catch (err) {
-        console.error(err);
-        if (err.response) return alert(err.response.data);
-        alert(err);
+        console.error(err)
+        if (err.response) return alert(err.response.data)
+        alert(err)
       }
-      setLoading(false);
-    })();
-  }, []);
+      setLoading(false)
+    })()
+  }, [])
+
+  useEffect(() => {
+    ;(async () => {
+      setLoading(true)
+      try {
+        const { data } = await axios.get(
+          `${SERVER_URL}/api/transaction/${User}`,
+        )
+        setExpenseData(data)
+      } catch (err) {
+        console.error(err)
+        if (err.response) return alert(err.response.data)
+        alert(err)
+      }
+      setLoading(false)
+    })()
+  }, [])
 
   const handleFileUpload = async () => {
     const result = await DocumentPicker.getDocumentAsync({
-      type: "application/vnd.ms-excel",
+      type: 'application/vnd.ms-excel',
       copyToCacheDirectory: true,
-    });
+    })
 
-    if (result.type === "success") {
+    if (result.type === 'success') {
       // Handle the selected file here
-      console.log(result.uri, result.name, result.size);
+      console.log(result.uri, result.name, result.size)
     }
-  };
+  }
 
   const [radioButtons, setRadioButtons] = useState([
     {
-      id: "1",
-      label: "Food",
-      value: "food",
+      id: '1',
+      label: 'Food',
+      value: 'food',
     },
     {
-      id: "2",
-      label: "Travel",
-      value: "travel",
+      id: '2',
+      label: 'Travel',
+      value: 'travel',
     },
     {
-      id: "3",
-      label: "Entertainment",
-      value: "entertainment",
+      id: '3',
+      label: 'Entertainment',
+      value: 'entertainment',
     },
     {
-      id: "4",
-      label: "Miscellaneous",
-      value: "miscellaneous",
+      id: '4',
+      label: 'Miscellaneous',
+      value: 'miscellaneous',
     },
-  ]);
+  ])
 
   function onPressRadioButton(radioButtonsArray) {
-    setRadioButtons(radioButtonsArray);
+    setRadioButtons(radioButtonsArray)
     radioButtonsArray.map((radioButton) => {
       if (radioButton.selected) {
         setExpense({
           ...expense,
           type_of_transaction: radioButton.value,
-        });
+        })
       }
-    });
+    })
   }
 
   const handleSubmit = async (type) => {
-    if (type === "debit") {
-      expense.amount = expense.amount * -1;
+    setLoading(true)
+    expense.type = type
+    try {
+      await axios.post(`${SERVER_URL}/api/register/transaction`, expense)
+      setExpense({
+        amount: '',
+        user_id: User,
+      })
+      Alert.alert('Expense Added Successfully')
+      setModalVisible(false)
+    } catch (err) {
+      console.error(err)
+      if (err.response) return alert(err.response.data)
+      alert(err)
     }
-    console.log(expense);
-    // setLoading(true);
-    // try {
-    //   await axios.post(`${SERVER_URL}/api/register/gigs`, expense);
-    //   setExpense({
-    //     amount: "",
-    //     type_of_transaction: "",
-    //     user_id: User,
-    //   });
-    //   Alert.alert("Gig Added Successfully");
-    //   setModalVisible(false);
-    //   setRefetch(!refetch);
-    // } catch (err) {
-    //   console.error(err);
-    //   if (err.response) return alert(err.response.data);
-    //   alert(err);
-    // }
-    // setLoading(false);
-    setExpense({
-      amount: "",
-      user_id: User,
-    });
-    setModalVisible(false);
-  };
+    setLoading(false)
+  }
 
   return (
     <SafeAreaView style={{ ...CommonStyles.container }}>
@@ -142,85 +139,114 @@ const Tracker = () => {
         <View style={{ padding: 30 }}>
           <View
             style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              flexDirection: "row",
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              flexDirection: 'row',
             }}
           >
             <Text style={{ ...CommonStyles.title }}>Hello {userData.name}</Text>
-            <TouchableOpacity onPress={() => Alert.alert("logout")}>
+            <TouchableOpacity onPress={() => Alert.alert('logout')}>
               <Text>Logout</Text>
             </TouchableOpacity>
           </View>
 
           <TouchableOpacity>
-            <Text style={{ fontWeight: "bold", color: "blue" }}>
+            <Text style={{ fontWeight: 'bold', color: 'blue' }}>
               {`Set Budget  `}
               <FontAwesomeIcon name="plus" size={10} color="blue" />
             </Text>
           </TouchableOpacity>
 
           <View style={{ ...CommonStyles.card, padding: 30, marginTop: 35 }}>
-            <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+            <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
               Your Next Month Plan is :
             </Text>
             <View>
               <View
                 style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  flexDirection: "row",
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  flexDirection: 'row',
                   marginTop: 20,
                 }}
               >
                 <View>
-                  <Text style={styles.mainData}>₹5000</Text>
+                  <Text style={styles.mainData}>
+                    ₹
+                    {predictExpenseData.food
+                      ? predictExpenseData.food[0].amount
+                      : 0}
+                  </Text>
                   <Text style={styles.minorTitle}>Food</Text>
                 </View>
                 <View>
-                  <Text style={styles.mainData}>₹5000</Text>
+                  <Text style={styles.mainData}>
+                    ₹
+                    {predictExpenseData.travel
+                      ? predictExpenseData.travel[0].amount
+                      : 0}
+                  </Text>
                   <Text style={styles.minorTitle}>Transport</Text>
                 </View>
               </View>
               <View
                 style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  flexDirection: "row",
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  flexDirection: 'row',
                   marginTop: 30,
                 }}
               >
                 <View>
-                  <Text style={styles.mainData}>₹5000</Text>
+                  <Text style={styles.mainData}>
+                    ₹
+                    {predictExpenseData.entertainment
+                      ? predictExpenseData.entertainment[0].amount
+                      : 0}
+                  </Text>
                   <Text style={styles.minorTitle}>Entertainment</Text>
                 </View>
                 <View>
-                  <Text style={styles.mainData}>₹5000</Text>
+                  <Text style={styles.mainData}>
+                    ₹
+                    {predictExpenseData.miscellaneous
+                      ? predictExpenseData.miscellaneous[0].amount
+                      : 0}
+                  </Text>
                   <Text style={styles.minorTitle}>Miscellaneous</Text>
                 </View>
               </View>
             </View>
-            <Text style={{ marginTop: 30, fontWeight: "bold" }}>
-              Your Total Saving for next month will be : ₹600
+            <Text style={{ marginTop: 30, fontWeight: 'bold' }}>
+              Your Total Saving for next month will be : ₹
+              {predictExpenseData.pocket_money &&
+              predictExpenseData.food &&
+              predictExpenseData.travel &&
+              predictExpenseData.entertainment &&
+              predictExpenseData.miscellaneous
+                ? predictExpenseData.pocket_money[0] -
+                  (predictExpenseData.food[0].amount +
+                    predictExpenseData.travel[0].amount +
+                    predictExpenseData.entertainment[0].amount +
+                    predictExpenseData.miscellaneous[0].amount)
+                : 0}
             </Text>
           </View>
-
           <TouchableOpacity
             style={{
               ...CommonStyles.blueBtn,
-              alignItems: "center",
+              alignItems: 'center',
               marginBottom: 20,
             }}
             onPress={handleFileUpload}
           >
-            <Text style={{ color: "#fff", fontWeight: "bold", padding: 5 }}>
+            <Text style={{ color: '#fff', fontWeight: 'bold', padding: 5 }}>
               Uplaod your monthly statement
             </Text>
           </TouchableOpacity>
-
           <View
             style={{
               ...CommonStyles.card,
@@ -229,23 +255,23 @@ const Tracker = () => {
               maxHeight: 600,
             }}
           >
-            <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+            <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
               Your Recent Transactions :
             </Text>
             <FlatList
               showsVerticalScrollIndicator={false}
-              data={transactions}
+              data={expenseData}
               style={{ marginTop: 10 }}
               keyExtractor={(item, index) => index}
               renderItem={({ item }) => (
                 <View
                   style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    flexDirection: "row",
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    flexDirection: 'row',
                     marginTop: 20,
-                    backgroundColor: "#f2f2f2",
+                    backgroundColor: '#f2f2f2',
                     padding: 12,
                     borderRadius: 10,
                   }}
@@ -254,11 +280,11 @@ const Tracker = () => {
                     <Text
                       style={{
                         ...CommonStyles.transactionSub,
-                        textAlign: "left",
+                        textAlign: 'left',
                         marginBottom: 5,
                       }}
                     >
-                      {item.category}
+                      {item.type_of_transaction}
                     </Text>
                     <Text style={CommonStyles.transactionTitle}>
                       {item.date}
@@ -295,19 +321,19 @@ const Tracker = () => {
         <View
           style={{
             flex: 1,
-            justifyContent: "center",
-            backgroundColor: "#00000080",
-            alignItems: "center",
+            justifyContent: 'center',
+            backgroundColor: '#00000080',
+            alignItems: 'center',
           }}
         >
           <View
             style={{
-              backgroundColor: "#fff",
+              backgroundColor: '#fff',
               padding: 20,
-              width: "85%",
+              width: '85%',
               borderRadius: 20,
               elevation: 5,
-              shadowColor: "#c6c6c678",
+              shadowColor: '#c6c6c678',
               marginVertical: 5,
               shadowOffset: {
                 width: 0,
@@ -317,14 +343,14 @@ const Tracker = () => {
           >
             <View
               style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                backgroundColor: "#fff",
-                width: "100%",
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                backgroundColor: '#fff',
+                width: '100%',
               }}
             >
-              <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+              <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
                 Add a Transaction
               </Text>
               <TouchableOpacity
@@ -349,11 +375,11 @@ const Tracker = () => {
                 }
               />
             </View>
-            <View style={{ alignItems: "flex-start" }}>
+            <View style={{ alignItems: 'flex-start' }}>
               <RadioGroup
                 radioButtons={radioButtons}
                 onPress={onPressRadioButton}
-                containerStyle={{ alignItems: "flex-start", marginTop: 5 }}
+                containerStyle={{ alignItems: 'flex-start', marginTop: 5 }}
                 value={expense.category}
               />
             </View>
@@ -361,25 +387,25 @@ const Tracker = () => {
             <TouchableOpacity
               style={{
                 ...CommonStyles.blueBtn,
-                backgroundColor: "green",
-                alignItems: "center",
+                backgroundColor: 'green',
+                alignItems: 'center',
                 marginTop: 30,
               }}
-              onPress={() => handleSubmit("credit")}
+              onPress={() => handleSubmit('credit')}
             >
-              <Text style={{ fontWeight: "bold", color: "#fff", padding: 2 }}>
+              <Text style={{ fontWeight: 'bold', color: '#fff', padding: 2 }}>
                 Credit
               </Text>
             </TouchableOpacity>
             <TouchableOpacity
               style={{
                 ...CommonStyles.redBtn,
-                alignItems: "center",
+                alignItems: 'center',
                 marginTop: 10,
               }}
-              onPress={() => handleSubmit("debit")}
+              onPress={() => handleSubmit('debit')}
             >
-              <Text style={{ fontWeight: "bold", color: "#fff", padding: 2 }}>
+              <Text style={{ fontWeight: 'bold', color: '#fff', padding: 2 }}>
                 Debit
               </Text>
             </TouchableOpacity>
@@ -387,19 +413,19 @@ const Tracker = () => {
         </View>
       </Modal>
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default Tracker;
+export default Tracker
 
 const styles = StyleSheet.create({
   mainData: {
     fontSize: 25,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   minorTitle: {
     fontSize: 14,
-    textAlign: "center",
+    textAlign: 'center',
     marginTop: 5,
   },
-});
+})
