7eea3196daefca790212db8ea386481ae5082da9
adi50920 <adityarai50920@gmail.com>
2023-04-09 10:40:58 +0530

    Update Income

--- routes/Income.js
+++ routes/Income.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { body } from 'express-validator'
 import IncomeSchema from '../models/Income.js'
+import TransactionSchema from '../models/Transaction.js'
 
 const router = express.Router()
 
@@ -40,6 +41,60 @@ router.get('/income/:user_id', async (req, res) => {
       owner_id: user_id,
     }).lean()
     res.send(income_response)
+  } catch (error) {
+    console.log(error)
+    res.status(400).send(error.message)
+  }
+})
+
+router.get('/left_amount/:user_id', async (req, res) => {
+  const { user_id } = req.params
+  try {
+    const debit_response = await TransactionSchema.aggregate([
+      {
+        $match: {
+          owner_id: user_id,
+          type: 'debit',
+        },
+      },
+      {
+        $group: {
+          _id: '$owner_id',
+          total: {
+            $sum: '$amount',
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          total: 1,
+        },
+      },
+    ])
+    const credit_response = await TransactionSchema.aggregate([
+      {
+        $match: {
+          owner_id: user_id,
+          type: 'credit',
+        },
+      },
+      {
+        $group: {
+          _id: '$owner_id',
+          total: {
+            $sum: '$amount',
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          total: 1,
+        },
+      },
+    ])
+    res.json({ credit_response, debit_response })
   } catch (error) {
     console.log(error)
     res.status(400).send(error.message)
